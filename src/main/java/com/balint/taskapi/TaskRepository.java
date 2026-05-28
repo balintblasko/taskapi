2240317f56c5cc079a674fb4433a3c50388eca18
balintblasko <balint.blasko.de@gmail.com>
2026-05-28 15:58:09 +0200

    Clean repository imports

--- src/main/java/com/balint/taskapi/TaskRepository.java
+++ src/main/java/com/balint/taskapi/TaskRepository.java
@@ -1,8 +1,5 @@
 package com.balint.taskapi;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
